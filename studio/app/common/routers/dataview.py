c90f1feb88746d497cbcf01afc7f45f53f308ff4
itutu-tienday <>
2025-07-11 15:40:49 +0900

    Added display of relational data in experiment_records

--- studio/app/common/routers/dataview.py
+++ studio/app/common/routers/dataview.py
@@ -1,21 +1,19 @@
-from typing import List, Optional, Sequence
+from typing import Optional, Sequence
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from fastapi_pagination.ext.sqlmodel import paginate
 from sqlalchemy.sql import Select
 from sqlmodel import Session, select
 
-from studio.app.common import models as common_model
+from studio.app.common import models
 from studio.app.common.core.auth.auth_dependencies import get_current_user
 from studio.app.common.core.logger import AppLogger
 from studio.app.common.db.database import get_db
-from studio.app.common.schemas.base import SortDirection, SortOptions
+from studio.app.common.schemas.base import SortOptions
 from studio.app.common.schemas.dataview import (
     DataviewRecord,
-    DataviewRecordHeader,
     DataviewRecordSearchOptions,
     PageWithHeader,
-    PublishFlags,
 )
 from studio.app.common.schemas.users import User
 
@@ -25,30 +23,42 @@ public_router = APIRouter(tags=["Dataview"])
 logger = AppLogger.get_logger()
 
 
-def experiment_transformer(items: Sequence) -> Sequence:
-    experiments = []
+def records_pagenate_transformer(items: Sequence) -> Sequence:
+    records = []
 
     for item in items:
-        # TODO: Planned to implement
-        pass
+        record = DataviewRecord.from_orm(item)
 
-    return experiments
+        # Adjusting response fields
+        record.owner = record.workspace.user
+        record.workspace.user = None
+
+        records.append(record)
+
+    return records
 
 
 def get_search_db_experiment_query(
     query: Select, options: DataviewRecordSearchOptions
 ) -> Select:
-    if options.uid is not None:
+    if options.uid:
         query = query.filter(
-            common_model.ExperimentRecord.uid.like("%{0}%".format(options.uid))
+            models.ExperimentRecord.uid.like("%{0}%".format(options.uid))
+        )
+
+    if options.user_name:
+        query = query.filter(models.User.name.like("%{0}%".format(options.user_name)))
+
+    if options.workspace_name:
+        query = query.filter(
+            models.Workspace.name.like("%{0}%".format(options.workspace_name))
         )
 
     return query
 
 
 @router.get(
-    # "/dataview",
-    "/expdb/experiments",
+    "/dataview",
     response_model=PageWithHeader[DataviewRecord],
     description="""
 - Search and respond to data for display in Dataview
@@ -62,24 +72,43 @@ async def search_dataview_records(
     current_user: User = Depends(get_current_user),
 ):
     sa_sort_list = sortOptions.get_sa_sort_list(
-        sa_table=common_model.ExperimentRecord,
-        mapping={},
+        sa_table=models.ExperimentRecord,
+        mapping={
+            "user_name": models.User.name,
+            "workspace_name": models.Workspace.name,
+            "last_modified": models.ExperimentRecord.updated_at,
+        },
     )
-    query = select(common_model.ExperimentRecord)
+
+    query = (
+        select(models.ExperimentRecord)
+        .join(
+            models.Workspace,
+            models.Workspace.id == models.ExperimentRecord.workspace_id,
+        )
+        .join(
+            models.User,
+            models.User.id == models.Workspace.user_id,
+        )
+        .filter(
+            models.Workspace.deleted.is_(False),
+            models.User.id == current_user.id,
+            models.User.active.is_(True),
+        )
+    )
 
     query = get_search_db_experiment_query(query, options)
 
     # TODO: Planned to implement
     # if publish_status is not None:
-    #    query = query.filter(common_model.ExperimentRecord.publish_status == publish_status)
+    #    query = query.filter(models.ExperimentRecord.publish_status == publish_status)
 
-    query = query.group_by(common_model.ExperimentRecord.id).order_by(*sa_sort_list)
+    query = query.group_by(models.ExperimentRecord.id).order_by(*sa_sort_list)
 
     data: PageWithHeader = paginate(
         session=db,
         query=query,
-        # transformer=experiment_transformer, # TODO: Planned to implement
-        transformer=None,  # TODO: Planned to implement
+        transformer=records_pagenate_transformer,
     )
 
     return data
