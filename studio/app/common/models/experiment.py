d541e2762dfc3957cc1f91dc0e41e88b61b6b1dd
itutu-tienday <>
2025-07-14 14:05:52 +0900

    dataview feature: - Added record publishing function

--- studio/app/common/models/experiment.py
+++ studio/app/common/models/experiment.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
+from sqlalchemy import Integer
 from sqlalchemy.dialects.mysql import BIGINT
 from sqlmodel import Column, Field, ForeignKey, Relationship, String
 
 from studio.app.common.models.base import Base, TimestampMixin
+from studio.app.common.schemas.dataview import PublishStatus
 
 
 class ExperimentRecord(Base, TimestampMixin, table=True):
@@ -15,11 +17,21 @@ class ExperimentRecord(Base, TimestampMixin, table=True):
         ),
     )
     uid: str = Field(sa_column=Column(String(100), nullable=False, index=True))
+
     data_usage: int = Field(
         sa_column=Column(
             BIGINT(unsigned=True), nullable=False, comment="data usage in bytes"
         ),
         default=0,
+    )
+
+    publish_status: int = Field(
+        sa_column=Column(
+            Integer(),
+            nullable=False,
+            default=PublishStatus.off.value,
+            comment="0: private, 1: public",
+        )
     )
 
     workspace: Optional["Workspace"] = Relationship(  # noqa: F821
