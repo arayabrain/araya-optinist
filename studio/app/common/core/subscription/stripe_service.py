e0edea67c548143d8f49eb88843013d4415ecedb
tsuchiyama-araya <tsuchiyama_yutaka@araya.org>
2025-11-18 16:02:22 +0900

    Remove Trial subscription implementation -> This will be added on different branch

--- studio/app/common/core/subscription/stripe_service.py
+++ studio/app/common/core/subscription/stripe_service.py
@@ -29,7 +29,6 @@ class StripeSubscriptionStatus(StrEnum):
 
     INCOMPLETE = "incomplete"
     INCOMPLETE_EXPIRED = "incomplete_expired"
-    TRIALING = "trialing"
     ACTIVE = "active"
     PAST_DUE = "past_due"
     CANCELED = "canceled"
