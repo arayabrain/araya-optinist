f8a6cb3f76d3ea47303e52d98eebb44d2c99a327
Miles <miles_desforges@araya.org>
2025-11-27 13:54:56 +0900

    Comment out custom node

--- studio/app/optinist/wrappers/__init__.py
+++ studio/app/optinist/wrappers/__init__.py
@@ -1,5 +1,6 @@
 from studio.app.optinist.wrappers.caiman import caiman_wrapper_dict
-from studio.app.optinist.wrappers.custom import custom_wrapper_dict
+
+# from studio.app.optinist.wrappers.custom import custom_wrapper_dict
 from studio.app.optinist.wrappers.data_utils import utils_wrapper_dict
 from studio.app.optinist.wrappers.lccd import lccd_wrapper_dict
 from studio.app.optinist.wrappers.maintenance import maintenance_wrapper_dict
@@ -13,4 +14,4 @@ wrapper_dict.update(**lccd_wrapper_dict)
 wrapper_dict.update(**optinist_wrapper_dict)
 wrapper_dict.update(**utils_wrapper_dict)
 wrapper_dict.update(**maintenance_wrapper_dict)
-wrapper_dict.update(**custom_wrapper_dict)
+# wrapper_dict.update(**custom_wrapper_dict)
