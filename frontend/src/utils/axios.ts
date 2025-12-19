c8bb9f0eade2629df55fc904f729824a3f51f08e
Miles <miles_desforges@araya.org>
2025-12-19 19:16:47 +0900

    Fix duplication that occured in merge

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -19,12 +19,6 @@ interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
   _retryWithoutPremium?: boolean
 }
 
-// Extend AxiosRequestConfig to include custom retry property
-interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
-  _retry?: boolean
-  _retryWithoutPremium?: boolean
-}
-
 const axios = axiosLibrary.create({
   baseURL: BASE_URL,
   timeout: 600000,
