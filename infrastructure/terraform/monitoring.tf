c4ad98431abf5763b66a99777299197f917f3523
Miles <miles_desforges@araya.org>
2025-12-19 10:55:24 +0900

    Uncomment out monitoring in main branch

--- infrastructure/terraform/monitoring.tf
+++ infrastructure/terraform/monitoring.tf
@@ -393,8 +393,8 @@ resource "aws_cloudwatch_dashboard" "main" {
           metrics = [
             ["OptiNiSt/Premium", "ActiveAssignments", { "label" : "Active Premium Users" }],
             ["OptiNiSt/Premium", "InstanceUtilization", { "label" : "Premium Instance Utilization %" }],
-#            ["AWS/Lambda", "Duration", "FunctionName", aws_lambda_function.premium_manager.function_name, { "label" : "Premium Manager Duration" }],
-#            ["AWS/Lambda", "Errors", "FunctionName", aws_lambda_function.premium_manager.function_name, { "label" : "Premium Manager Errors" }]
+            ["AWS/Lambda", "Duration", "FunctionName", aws_lambda_function.premium_manager.function_name, { "label" : "Premium Manager Duration" }],
+            ["AWS/Lambda", "Errors", "FunctionName", aws_lambda_function.premium_manager.function_name, { "label" : "Premium Manager Errors" }]
           ]
           view    = "timeSeries"
           stacked = false
