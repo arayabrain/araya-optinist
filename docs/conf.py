a1d5fb642ae02060b6dc0d8ade30193fb7bc1aab
itutu-tienday <>
2026-03-09 10:46:43 +0900

    update docs version

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ from datetime import datetime
 project = "Araya-OptiNiSt Cloud"
 copyright = f"{datetime.now().year}, Araya Inc., OIST"
 author = "Araya Inc."
-release = "2.4.0"
+release = "1.0.0"
 
 # -- readthedocs -------------------------------------------------------------
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
